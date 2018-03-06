8ab2793d1b24c3fba98d3e327fb13f45c381abff
savov <savov@hotmail.com>
2018-03-06 08:18:52 -0800

    Reimplementing WordCount as topology.

--- src/WordCount.scala
+++ src/WordCount.scala
@@ -2,30 +2,65 @@ import java.sql.Timestamp
 
 import scala.reflect.runtime.universe._
 
-import scala.io.StdIn
-object WordCount extends App {
+/**
+  * Word count topology.
+  */
+class WordCount extends AbstractTopology[String, (String, Int)] {
+
   val countWords = new CountWords
   val splitWords = new SplitWords
 
-  // TODO: Read type annotations and spin windowing threads or counters.
+  override def init(): Unit = {
+    super.init()
+    countWords.init(); splitWords.init()
+  }
 
-  countWords.init(); splitWords.init()
+  override def activate(sink: ((String, Int), Timestamp) => Unit): Unit = {
+    super.activate(sink)
+
+    // Link & activate
+    countWords.activate(sink)
+    splitWords.activate(countWords.process)
+  }
+
+  /**
+    * Process individual event.
+    * This may or may not result in output event sent to the sink.
+    * Only active kernels will process events.
+    *
+    * @param ev event to process
+    * @param ts timestamp of the event
+    */
+  override def process(ev: String, ts: Timestamp): Unit = {
+    splitWords.process(ev, new Timestamp(System.currentTimeMillis()))
+  }
+
+  override def cleanup(): Unit = {
+    super.cleanup()
+    splitWords.cleanup(); countWords.cleanup()
+  }
+}
+
+import scala.io.StdIn
+object WordCount extends App {
+
+  // TODO: Read type annotations and spin windowing threads or counters.
 
   def topologyOutput(ev: (String, Int), ts: Timestamp): Unit = ev match {
     case (w, c) => println (s"$ts: $w -> $c")
   }
 
-  // Link & activate
-  countWords.activate(topologyOutput)
-  splitWords.activate(countWords.process)
-
-  def topologyInput(ev: String): Unit = splitWords.process(ev, new Timestamp(System.currentTimeMillis()))
+  // Initialize and activate the topology
+  val topology = new WordCount
+  topology.init()
+  topology.activate(topologyOutput)
 
   // Feed lines from stdin
   var line: String = _
   while ({ line = StdIn.readLine(); line} != null) {
-    topologyInput(line)
+    topology.process(line, new Timestamp(System.currentTimeMillis()))
   }
 
-  splitWords.cleanup(); countWords.cleanup()
+  // Cleanup & exit
+  topology.cleanup()
 }
